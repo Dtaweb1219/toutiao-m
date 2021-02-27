1688677997bde19927d663440f001760abee8584
duantangao <1257511300@qq.com>
2021-02-27 11:27:35 +0800

    07.实现基本登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,20 +1,96 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <!-- /导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        name="mobile"
+        placeholder="请输入手机号"
+        v-model="user.mobile"
+        required
+        clearable
+      >
+        <i slot="left-icon" class="iconfont toutiao-shouji"></i>
+      </van-field>
+      <van-field
+        v-model="user.code"
+        name="code"
+        type="number"
+        placeholder="请输入验证码"
+        required
+      >
+        <i slot="left-icon" class="iconfont toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
 </template>
 
 <script>
+import { login } from '@/api/user.js'
 export default {
   name: 'LoginPage',
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created() {},
   mounted() {},
-  methods: {}
+  methods: {
+    async onSubmit() {
+      const user = this.user
+      try {
+        const res = await login(user)
+        console.log('登录成功', res)
+      } catch (err) {
+        if (err.response.status === 400) {
+          console.log('登录失败', err)
+        }
+      }
+    }
+  }
 }
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-container {
+  .iconfont {
+    font-size: 37px;
+  }
+  .send-sms-btn {
+    width: 160px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
