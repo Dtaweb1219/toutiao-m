674418abbe413705bab047b6e0432e14076e4210
duantangao <1257511300@qq.com>
2021-02-28 14:21:25 +0800

    21.优化设置token

--- src/api/user.js
+++ src/api/user.js
@@ -1,37 +1,39 @@
 /**
  * 用户相关的请求模块
  */
+
 import request from '@/utils/request'
-import store from '@/store'
+// import store from '@/store'
 
 /**
  * 获取用户自己的信息
- */
-export const getUserInfo = () => {
-  return request({
-    method: 'GET',
-    url: '/app/v1_0/user',
-    // 发送请求头数据
-    headers: {
-      // 注意：该接口需要授权才能访问
-      //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
-      Authorization: `Bearer ${store.state.user.token}`
-    }
-  })
-}
-/**
- * 用户登录
  */
 export const login = data => {
   return request({
     method: 'POST',
     url: '/app/v1_0/authorizations',
-    data
+    data: data
   })
 }
 export const sendSms = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+/**
+ * 获取用户自己的信息
+ */
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user'
+    // 发送请求头数据
+    // headers: {
+    //   // 注意：该接口需要授权才能访问
+    //   //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
+    //   // 引用store的user数据要在上面先import store
+    //   // Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
