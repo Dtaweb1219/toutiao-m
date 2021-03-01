c15213f92792ebb3a2982eac4998bb488353adff
duantangao <1257511300@qq.com>
2021-03-01 14:41:03 +0800

    27.首页加载文章列表数据

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -24,14 +24,21 @@
       v-model="loading"
       :finished="finished"
       finished-text="没有更多了"
+      :error.sync="error"
+      error-text="请求失败，点击重新加载"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell
+        v-for="(article, index) in list"
+        :key="index"
+        :title="article.title"
+      />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getArticles } from '@/api/article'
 export default {
   name: 'ArticleList',
   components: {},
@@ -43,9 +50,11 @@ export default {
   },
   data() {
     return {
-      list: [], // 存储列表数据的数组
-      loading: false, // 控制加载中 loading 状态
-      finished: false // 控制数据加载结束的状态
+      list: [], // 文章列表数据
+      loading: false, // 上拉加载更多的 loading 状态
+      finished: false, // 是否加载结束
+      error: false, // 是否加载失败
+      timestamp: null // 请求下一页数据的时间戳
     }
   },
   computed: {},
@@ -53,30 +62,32 @@ export default {
   created() {},
   mounted() {},
   methods: {
-    // 初始化或滚动到底部的时候会触发调用 onLoad
-    onLoad() {
-      console.log('onLoad')
-      // 1. 请求获取数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        // 2. 把请求结果数据放到 list 数组中
-        for (let i = 0; i < 10; i++) {
-          // 0 + 1 = 1
-          // 1 + 1 = 2
-          // 2 + 1 = 3
-          this.list.push(this.list.length + 1)
-        }
+    // 当触发上拉加载更多的时候调用该函数
+    async onLoad() {
+      try {
+        // 1. 请求获取数据
+        const { data } = await getArticles({
+          channel_id: this.channel.id, // 频道 id
+          timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
+          with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+        })
 
-        // 3. 本次数据加载结束之后要把加载状态设置为结束
-        //     loading 关闭以后才能触发下一次的加载更多
+        // 2. 把数据添加到 list 数组中
+        const { results } = data.data
+        this.list.push(...results)
+
+        // 3. 设置本次加载中 loading 状态结束
         this.loading = false
 
-        // 4. 判断数据是否全部加载完成
-        if (this.list.length >= 40) {
-          // 如果没有数据了，把 finished 设置为 true，之后不再触发加载更多
+        // 4. 判断数据是否加载结束
+        if (results.length) {
+          // 更新获取下一页数据的时间戳
+          this.timestamp = data.data.pre_timestamp
+        } else {
+          // 没有数据了，设置加载状态结束，不再触发上拉加载更多了
           this.finished = true
         }
-      }, 1000)
+      } catch (err) {}
     }
   }
 }
