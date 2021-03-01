9263c356db5a5a32e8f21e40f82f0ce6b612824a
duantangao <1257511300@qq.com>
2021-03-01 15:14:10 +0800

    29.首页文章列表头部固定定位

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
     <!-- 导航栏 -->
-    <van-nav-bar class="page-nav-bar">
+    <van-nav-bar class="page-nav-bar" fixed>
       <van-button
         class="search-btn"
         slot="title"
@@ -67,9 +67,12 @@ export default {
 </script>
 
 <style scoped lang="less">
+// 当前组件中加了 scoped 对内部样式的修改需要加 /deep/，或者去掉 scoped
 .home-container {
+  // #3
+  padding-top: 174px;
   padding-bottom: 100px;
-  /deep/.van-nav-bar__title {
+  /deep/ .van-nav-bar__title {
     max-width: unset;
   }
   .search-btn {
@@ -82,32 +85,63 @@ export default {
       font-size: 32px;
     }
   }
-  .placeholder {
-    flex-shrink: 0;
-    width: 66px;
-    height: 82px;
-  }
-  .hamburger-btn {
-    position: fixed;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 66px;
-    height: 82px;
-    background-color: #fff;
-    opacity: 0.902;
-    i.toutiao {
-      font-size: 33px;
-    }
-    &:before {
-      content: '';
-      position: absolute;
+  /deep/ .channel-tabs {
+    // #2
+    .van-tabs__wrap {
+      position: fixed;
+      top: 92px;
       left: 0;
-      width: 1px;
-      height: 100%;
-      background-image: url(~@/assets/gradient-gray-line.png);
-      background-size: contain;
+      right: 0;
+      z-index: 1;
+      height: 82px;
+    }
+    // Tab 标签页
+    .van-tab {
+      border-right: 1px solid #edeff3;
+      min-width: 200px;
+      font-size: 30px;
+      color: #777777;
+    }
+    .van-tab--active {
+      color: #333333;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0;
+    }
+    .van-tabs__line {
+      bottom: 8px;
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
+    }
+    // 汉堡
+    .placeholder {
+      flex-shrink: 0;
+      width: 66px;
+      height: 82px;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 66px;
+      height: 82px;
+      background-color: #fff;
+      background-color: rgba(255, 255, 255, 0.902);
+      i.iconfont {
+        font-size: 33px;
+      }
+      &:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 58px;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
     }
   }
 }
