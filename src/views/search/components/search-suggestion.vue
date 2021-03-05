c34beb7c046cabc4b2a93569621c199b39332849
duantangao <1257511300@qq.com>
2021-03-05 16:07:44 +0800

    55.文章搜索-请求获取展示数据

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="search-suggestion">
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
+    <van-cell
+      v-for="(item, index) in suggestions"
+      :key="index"
+      :title="item"
+      icon="search"
+    ></van-cell>
   </div>
 </template>
 
 <script>
+import { getSearchSuggestion } from '@/api/search.js'
 export default {
   name: 'SearchSuggestion',
   components: {},
@@ -19,13 +21,32 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      suggestions: [] // 联想建议存到这
+    }
   },
   computed: {},
-  watch: {},
+  watch: {
+    searchText: {
+      // 当 searchText发生改变的时候就会调用handler函数，这个函数名字是固定的
+      handler(val) {
+        this.loadSearchSuggestion(val)
+      },
+      immediate: true // 该回调将会再侦听开始之后就被立即调用
+    }
+  },
   created() {},
   mounted() {},
-  methods: {}
+  methods: {
+    async loadSearchSuggestion(q) {
+      try {
+        const { data } = await getSearchSuggestion(q)
+        this.suggestions = data.data.options
+      } catch {
+        this.$toast('获取失败')
+      }
+    }
+  }
 }
 </script>
 
