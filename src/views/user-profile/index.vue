518e9ea1e54be1dc0faaf05edb2f657add0a6174
duantangao <1257511300@qq.com>
2021-03-09 18:37:37 +0800

    100.展示用户信息

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -10,22 +10,41 @@
 
     <!-- 个人信息 -->
     <van-cell title="头像" is-link>
-      <van-image
-        class="avatar"
-        fit="cover"
-        round
-        src="https://img01.yzcdn.cn/vant/cat.jpeg"
-      />
+      <van-image class="avatar" fit="cover" round :src="user.photo" />
     </van-cell>
-    <van-cell title="昵称" value="内容" is-link></van-cell>
-    <van-cell title="性别" value="内容" is-link></van-cell>
-    <van-cell title="生日" value="内容" is-link></van-cell>
+    <van-cell title="昵称" :value="user.name" is-link></van-cell>
+    <van-cell
+      title="性别"
+      :value="user.gender === 0 ? '男' : '女'"
+      is-link
+    ></van-cell>
+    <van-cell title="生日" :value="user.birthday" is-link></van-cell>
   </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/user'
 export default {
-  name: 'UserProfile'
+  name: 'UserProfile',
+  data() {
+    return {
+      user: {} // 个人信息
+    }
+  },
+  created() {
+    this.loadUserProfile()
+  },
+  methods: {
+    async loadUserProfile() {
+      try {
+        const { data } = await getUserProfile()
+        // console.log(data)
+        this.user = data.data
+      } catch (err) {
+        this.$toast('数据获取失败')
+      }
+    }
+  }
 }
 </script>
 
